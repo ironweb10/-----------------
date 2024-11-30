4615cf44bcf05c3144a07f664576844ce97bf88f
ironweb10 <97882381+ironweb10@users.noreply.github.com>
2024-11-30 23:02:05 +0100

    Fixed Update Playlists Bugs

--- index.js
+++ index.js
@@ -1173,10 +1173,10 @@ async function updatePlaylists() {
         const playlists = response.data.data;
 
        
-        const playlistIds = playlists.map(playlist => playlist.id);
+        const playlistIds = playlists.map(playlist => playlist.id.toLowerCase());
 
       
-        const content = `module.exports.allowedPlaylists = Object.freeze([\n    "${playlistIds.join('",\n    "')}"\n]);\n\nmodule.exports.websocketHeaders = Object.freeze({\n   'Accept-Version': '*',\n   'Pragma': 'no-cache',\n   'Cache-Control': 'no-cache'\n});\n`;
+        const content = `module.exports.allowedPlaylists = Object.freeze([\n    "${playlistIds.join('",\n    "')}"\n])\n\nmodule.exports.websocketHeaders = Object.freeze({\n   'Accept-Version': '*',\n   'Pragma': 'no-cache',\n   'Cache-Control': 'no-cache'\n})\n`;
 
        
         fs.writeFileSync('utils/constants.js', content, 'utf8');
