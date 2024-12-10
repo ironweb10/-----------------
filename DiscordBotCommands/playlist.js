ccaf17fd37f4398faf8c9dc6770a3b4260c9336c
ironweb10 <97882381+ironweb10@users.noreply.github.com>
2024-12-10 20:32:38 +0100

    Fixed playlist command

--- DiscordBotCommands/playlist.js
+++ DiscordBotCommands/playlist.js
@@ -1,28 +1,38 @@
-
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { MessageEmbed } = require('discord.js');
 
-
-
-
 module.exports = {
-    data: new SlashCommandBuilder()
-      .setName('playlist')
-      .setDescription('Sets the current playlist if the bot is party leader')
-      .addStringOption(option =>
-        option.setName('playlist')
-          .setDescription('Playlist to set')
-          .setRequired(true)
-      ),
-    async execute(interaction, botClient) {
-      const setplaylist = interaction.options.getString('playlist');
-      botClient.party.setPlaylist({ playlistName: setplaylist });
+  data: new SlashCommandBuilder()
+    .setName('playlist')
+    .setDescription('Sets the current playlist if the bot is party leader')
+    .addStringOption(option =>
+      option.setName('mnemonic')
+        .setDescription('The new mnemonic (Playlist id or island code)')
+        .setRequired(true)
+    ),
+  async execute(interaction, botClient) {
+    const mnemonic = interaction.options.getString('mnemonic');
+   
+
+    try {
+     
+      await botClient.party.setPlaylist(mnemonic); //I was looking for this for more than 1 year siuuuuuuuuuuuuuuuuuuuuuuuuuuuuuu
+
       const embed = new MessageEmbed()
         .setColor('#00FF00')
-        .setTitle('Playlist Set')
-        .setDescription(`Playlist Id: ${setplaylist} has been set as the playlist!`)
+        .setTitle('Playlist Updated')
+        .setDescription(`Playlist set to: ${mnemonic}`)
+        
         .setTimestamp();
+
       await interaction.reply({ embeds: [embed] });
+    } catch (error) {
+      const errorEmbed = new MessageEmbed()
+        .setColor('#FF0000')
+        .setTitle('Error')
+        .setDescription(`Failed to update playlist: ${error.message}`)
+        .setTimestamp();
+      await interaction.reply({ embeds: [errorEmbed] });
     }
-  };
-  
+  }
+};
